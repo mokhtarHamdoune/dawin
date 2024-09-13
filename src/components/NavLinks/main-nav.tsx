421ff396592d2d0919e66f3c8a032c8178b9eb40
mokhtarHamdoune <mokhtardark@gmail.com>
2024-09-13 18:15:30 +0100

    feat change favicon of the app; fix some errors related to key

--- src/components/NavLinks/main-nav.tsx
+++ src/components/NavLinks/main-nav.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 
 export function MainNav() {
     return (
-        <nav className="gap-x-6 hidden md:flex items-center">
+        <nav className="gap-x-6 hidden md:flex items-center bg-white">
             {
                 navItems.map(item => (
-                    <Link href={item.href} className="flex items-center p-2 hover:bg-secondary rounded-md">{item.label}
+                    <Link key={item.label} href={item.href} className="flex items-center p-2 hover:bg-secondary rounded-md">{item.label}
                         <span className="block ml-0.5">
                             <item.icon size={16} />
                         </span>
--- src/components/NavLinks/mobile-nav.tsx
+++ src/components/NavLinks/mobile-nav.tsx
@@ -8,10 +8,10 @@ type MobilNavProps = {
 export function MobilNav({ visible }: MobilNavProps) {
     return (
         <div className="absolute w-full">
-            <nav className={`${visible ? "block" : "hidden"} md:hidden border-t border-l  border-r border-slate-200`}>
+            <nav className={`${visible ? "block" : "hidden"} md:hidden  bg-white border-t border-l  border-r border-slate-200`}>
                 {
                     navItems.map(item => (
-                        <Link href={item.href} className="flex items-center gap-x-1 px-2 py-2 hover:bg-secondary border-b border-slate-200"><item.icon size={18} /><span>{item.label}</span> </Link>
+                        <Link key={item.label} href={item.href} className="flex items-center gap-x-1 px-2 py-2 hover:bg-secondary border-b border-slate-200"><item.icon size={18} /><span>{item.label}</span> </Link>
                     ))
                 }
             </nav>
